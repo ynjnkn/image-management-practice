ed338115c723c2c3c685e56161378a12fa71310b
ynjnkn <yjun_k@hotmail.com>
2022-04-14 11:44:35 +0900

    01. Node.js - 백엔드로 이미지 업로드 받기_이미지 저장 전 필터 걸어주기 [완료]

--- server/routes/uploadRouter.js
+++ server/routes/uploadRouter.js
@@ -1,19 +1,38 @@
 // Dependencies
 const { Router } = require("express");
 const uploadRouter = Router();
+const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
-const multer = require("multer");
+
+// Multer Configurations
+const acceptedImageFileFormats = [
+  "image/apng",
+  "image/avif",
+  "image/gif",
+  "image/jpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/webp",
+];
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "./uploads"),
   filename: (req, file, cb) =>
     cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
 });
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (acceptedImageFileFormats.includes(file.mimetype)) cb(null, true);
+    else cb(new Error("Invalid File Type"), false);
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 5, // ~5MB
+  },
+});
 
 uploadRouter.post("/", upload.single("imageTest"), async (req, res) => {
   try {
-    console.log(req.file);
     return res.status(200).json(req.file);
   } catch (error) {
     console.log({ error: { name: error.name, message: error.message } });
